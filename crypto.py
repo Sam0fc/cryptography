ba50e5645c482b2fdb9896cb6aa3cf3cf6feb0e1
root <root@Retina.localdomain>
2020-09-24 14:03:15 -0700

    Finished vignere (not commented)

--- crypto.py
+++ crypto.py
@@ -1,3 +1,6 @@
+ASCII_LETTER_START = 65
+LENGTH_ALPHABET = 26
+
 # Caesar Cipher
 # Arguments: string, integer
 # Returns: string
@@ -5,30 +8,59 @@ def encrypt_caesar(plaintext, offset):
     new_text = ""
     for letter in plaintext.upper():
         if letter.isalpha():
-            letter = chr(((ord(letter)-64)%26) + offset + 64)
+            numLetter = ord(letter) - ASCII_LETTER_START
+            offsetNumLetter = (numLetter + offset)%LENGTH_ALPHABET
+            letter = chr(offsetNumLetter + ASCII_LETTER_START)
         new_text += letter
     return new_text
 
 # Arguments: string, integer
 # Returns: string
 def decrypt_caesar(ciphertext, offset):
-    pass
+    new_text = ""
+    for letter in ciphertext.upper():
+        if letter.isalpha():
+            numLetter = ord(letter) - ASCII_LETTER_START
+            offsetNumLetter = (numLetter - offset)%LENGTH_ALPHABET
+            letter = chr(offsetNumLetter + ASCII_LETTER_START)
+        new_text += letter
+    return new_text
 
 # Vigenere Cipher
 # Arguments: string, string
 # Returns: string
 def encrypt_vigenere(plaintext, keyword):
-    pass
+    new_text = ""
+    keyword=keyword.upper()
+    plaintext=plaintext.upper()
+    for index,letter in enumerate(plaintext):
+        offset = ord(keyword[index%len(keyword)]) - ASCII_LETTER_START
+        if letter.isalpha():
+            numLetter = ord(letter) - ASCII_LETTER_START
+            offsetNumLetter = (numLetter + offset)%LENGTH_ALPHABET
+            letter = chr(offsetNumLetter + ASCII_LETTER_START)
+        new_text += letter
+    return new_text
 
 # Arguments: string, string
 # Returns: string
 def decrypt_vigenere(ciphertext, keyword):
-    pass
-
+    new_text = ""
+    keyword=keyword.upper()
+    ciphertext=ciphertext.upper()
+    for index,letter in enumerate(ciphertext):
+        offset = ord(keyword[index%len(keyword)]) - ASCII_LETTER_START
+        if letter.isalpha():
+            numLetter = ord(letter) - ASCII_LETTER_START
+            offsetNumLetter = (numLetter - offset)%LENGTH_ALPHABET
+            letter = chr(offsetNumLetter + ASCII_LETTER_START)
+        new_text += letter
+    return new_text
 # Merkle-Hellman Knapsack Cryptosystem
 # Arguments: integer
 # Returns: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 def generate_private_key(n=8):
+    
     pass
 
 # Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
@@ -47,7 +79,7 @@ def decrypt_mhkc(ciphertext, private_key):
     pass
 
 def main():
-    print(encrypt_caesar(" !\#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",1))
+    print(decrypt_vigenere(input(),input()))
     pass
 
 if __name__ == "__main__":
